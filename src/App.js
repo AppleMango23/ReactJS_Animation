4e7ccc0cda967ad31291e7a95e6d165efe4e5582
Noah Yek <ninjayek@gmail.com>
2021-07-05 19:20:09 +0800

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import { animated, useTransition } from "react-spring";
 
-function App() {
+const App = () => {
+  const [items, setItems] = useState([]);
+  const transition = useTransition(items, {
+    from: { x: -100, y: 800, opacity: 0, width: 10, height: 10 },
+    enter: (item) => async (next) => {
+      await next({ y: item.y, opacity: 1, delay: item.delay }); //making one by one coming in
+      await next({ x: 0, width: 100, height: 100 });
+    },
+    leave: { x: 100, y: 900, opacity: 0 },
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <button
+        onClick={() => {
+          setItems((v) =>
+            v.length
+              ? []
+              : [
+                  { y: -100, delay: 200 },
+                  { y: -50, delay: 400 },
+                  { y: 0, delay: 600 },
+                ]
+          ); //If it is true make it false else make it true
+        }}
+      >
+        {items.length ? "HIDE~" : "POP!"}
+      </button>
+
+      <div className="container">
+        {transition((style, item) =>
+          item ? (
+            <animated.div style={style} className="item">
+              {/* <p>I am Noah! React!</p> */}
+            </animated.div>
+          ) : (
+            ""
+          )
+        )}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
